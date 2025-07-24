7a28d3accecd0af970ac5efe8098ac91a9a44af0
Joshua <joshuazehms@gmail.com>
2025-07-24 15:31:08 -0700

    Current card almost done

--- src/Components/current-weather/CurrentWeather.js
+++ src/Components/current-weather/CurrentWeather.js
@@ -1,15 +1,45 @@
 import "./CurrentWeather.css";
 
 const CurrentWeather = () => {
-    return (
-        <div className="current-weather">
-            <div className="card-top">
-                <p className="city">Las Vegas</p>
-                <p className="weather-description">Sunny</p>
-            </div>
-            <img className="weather-icon" alt="weather"></img>
+  return (
+    <div className="current-weather">
+      <div className="card-top">
+        <div>
+          <p className="city">Las Vegas</p>
+          <p className="weather-description">Sunny</p>
         </div>
-    )
-}
+        <img
+          className="weather-icon"
+          alt="weather"
+          src="/icons/sunny.png"
+        ></img>
+      </div>
+      <div className="card-bottom">
+        <p className="temperature">90°F</p>
+        <div className="details">
+          <div className="parameter-row">
+            <span className="parameter-label">Details</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Feels like</span>
+            <span className="parameter-value">88°F</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Wind</span>
+            <span className="parameter-value">4 mph</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Humidity</span>
+            <span className="parameter-value">18%</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Pressure</span>
+            <span className="parameter-value">15 hPa</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default CurrentWeather;

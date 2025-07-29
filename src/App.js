2f5da0bbd8f8805a94f7d673f022b49e6f4203c9
Joshua <joshuazehms@gmail.com>
2025-07-29 12:03:06 -0700

    Remove imgs to use openweathers built in icons.
    Daily forecast working.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,44 @@
-import './App.css';
-import Search from './Components/search/Search';
-import CurrentWeather from './Components/current-weather/CurrentWeather';
+import "./App.css";
+import Search from "./Components/search/Search";
+import CurrentWeather from "./Components/current-weather/CurrentWeather";
+import Forecast from './Components/Forecast/forecast';
+import { weatherAPIURL, weatherAPIKey } from "./APIs/SearchAPI";
+import { useState } from "react";
 
 function App() {
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const [forecast, setForecast] = useState(null);
 
   const handleOnSearchChange = (searchData) => {
-    console.log(searchData);
-  }
+    const [lat, lon] = searchData.value.split(" ");
+
+    const currentWeatherFetch = fetch(
+      `${weatherAPIURL}?lat=${lat}&lon=${lon}&appid=${weatherAPIKey}&units=imperial`
+    );
+
+    const forecastFetch = fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${weatherAPIKey}&units=imperial`
+    );
+
+    Promise.all([currentWeatherFetch, forecastFetch])
+      .then(async ([weatherResponse, forecastResponse]) => {
+        const weatherData = await weatherResponse.json();
+        const forecastData = await forecastResponse.json();
+
+        setCurrentWeather({ city: searchData.label, ...weatherData });
+        setForecast({ city: searchData.label, ...forecastData });
+      })
+      .catch((error) => console.error("Error fetching data:", error));
+  };
+
+  console.log(currentWeather);
+  console.log(forecast);
 
   return (
     <div className="container">
-        <Search onSearchChange={handleOnSearchChange} />
-        <CurrentWeather/>
+      <Search onSearchChange={handleOnSearchChange} />
+      {currentWeather && <CurrentWeather data={currentWeather} />}
+      {forecast && <Forecast data={forecast} />}
     </div>
   );
 }
--- src/Components/current-weather/CurrentWeather.js
+++ src/Components/current-weather/CurrentWeather.js
@@ -1,45 +1,67 @@
 import "./CurrentWeather.css";
+import PropTypes from "prop-types";
 
-const CurrentWeather = () => {
+const CurrentWeather = ({ data }) => {
   return (
     <div className="current-weather">
       <div className="card-top">
         <div>
-          <p className="city">Las Vegas</p>
-          <p className="weather-description">Sunny</p>
+          <p className="city">{data.name}</p>
+          <p className="weather-description">{data.weather[0].description}</p>
         </div>
         <img
           className="weather-icon"
           alt="weather"
-          src="/icons/sunny.png"
-        ></img>
+          src={`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
+        />
       </div>
       <div className="card-bottom">
-        <p className="temperature">90°F</p>
+        <p className="temperature">{Math.round(data.main.temp)}°F</p>
         <div className="details">
           <div className="parameter-row">
             <span className="parameter-label">Details</span>
           </div>
           <div className="parameter-row">
             <span className="parameter-label">Feels like</span>
-            <span className="parameter-value">88°F</span>
+            <span className="parameter-value">{Math.round(data.main.feels_like)}°F</span>
           </div>
           <div className="parameter-row">
             <span className="parameter-label">Wind</span>
-            <span className="parameter-value">4 mph</span>
+            <span className="parameter-value">{data.wind.speed} mph</span>
           </div>
           <div className="parameter-row">
             <span className="parameter-label">Humidity</span>
-            <span className="parameter-value">18%</span>
+            <span className="parameter-value">{data.main.humidity}%</span>
           </div>
           <div className="parameter-row">
             <span className="parameter-label">Pressure</span>
-            <span className="parameter-value">15 hPa</span>
+            <span className="parameter-value">{data.main.pressure} hPa</span>
           </div>
         </div>
       </div>
     </div>
   );
+};
+
+CurrentWeather.propTypes = {
+  data: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    weather: PropTypes.arrayOf(
+      PropTypes.shape({
+        description: PropTypes.string.isRequired,
+        icon: PropTypes.string.isRequired,
+      })
+    ).isRequired,
+    main: PropTypes.shape({
+      temp: PropTypes.number.isRequired,
+      feels_like: PropTypes.number.isRequired,
+      humidity: PropTypes.number.isRequired,
+      pressure: PropTypes.number.isRequired,
+    }).isRequired,
+    wind: PropTypes.shape({
+      speed: PropTypes.number.isRequired,
+    }).isRequired,
+  }).isRequired,
 };
 
 export default CurrentWeather;
